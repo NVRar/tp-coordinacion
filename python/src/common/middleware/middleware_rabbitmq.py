aed7af2ab5de18ac0f9c7483a19edfbcf1375bda
Marcos Navarro Cafferata <mnavarroc@fi.uba.ar>
2026-04-12 10:41:30 -0300

    feat: Agregado de middleware rabbitmq

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,9 +6,59 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.queue_name = queue_name
+        self.channel.queue_declare(queue=queue_name, durable=True)
+
+    def close(self):
+        self.connection.close()
+
+    def send(self, message):
+        self.channel.basic_publish(exchange='', routing_key=self.queue_name, body=message, properties=pika.BasicProperties(delivery_mode=2))
+
+    def start_consuming(self, callback):
+        self.channel.basic_qos(prefetch_count=1)
+        def callback_wrapper(channel, method, properties, body):
+            ack = lambda: channel.basic_ack(delivery_tag=method.delivery_tag)
+            nack = lambda: channel.basic_nack(delivery_tag=method.delivery_tag)
+            callback(body, ack, nack)
+
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=callback_wrapper)
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
+  
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
-        pass
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
+        self.exchange_name = exchange_name
+        self.routing_keys = routing_keys
+        self.channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
+
+    def close(self):
+        self.connection.close()
+    
+    def send(self, message):
+        for routing_key in self.routing_keys:
+            self.channel.basic_publish(exchange=self.exchange_name, routing_key=routing_key, body=message)
+
+    def start_consuming(self, callback):
+        self.channel.basic_qos(prefetch_count=1)
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        queue_name = result.method.queue
+        for routing_key in self.routing_keys:
+            self.channel.queue_bind(exchange=self.exchange_name, queue=queue_name, routing_key=routing_key)
+        def callback_wrapper(channel, method, properties, body):
+            ack = lambda: channel.basic_ack(delivery_tag=method.delivery_tag)
+            nack = lambda: channel.basic_nack(delivery_tag=method.delivery_tag)
+            callback(body, ack, nack)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback_wrapper)
+        self.channel.start_consuming()
+
+    def stop_consuming(self):
+        self.channel.stop_consuming()
